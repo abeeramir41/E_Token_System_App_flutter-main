8ac0bd32678c44e6dc1782e60050b0afa3596196
abeer10 <abeeraamir10@gmail.com>
2021-04-23 15:19:56 +0500

    sub departments

--- lib/Home_Form.dart
+++ lib/Home_Form.dart
@@ -12,6 +12,7 @@ class InputToken extends StatefulWidget {
 class _InputTokenState extends State<InputToken> {
   String dropdownValue = 'Select';
   var selectedOrganization;
+  var selectedDepartment;
   TextEditingController name = TextEditingController();
   TextEditingController email = TextEditingController();
   TextEditingController phone = TextEditingController();
@@ -84,7 +85,61 @@ class _InputTokenState extends State<InputToken> {
 
     }
     ),
+               SizedBox(height: 10.0,),
+              StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance.collection("Organizations").doc(selectedOrganization).collection("department").snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData)
+                      return CircularProgressIndicator();
+                    else {
+                      List<DropdownMenuItem> department = [];
+                      for (int i = 0; i < snapshot.data.docs.length; i++) {
+                        DocumentSnapshot snap = snapshot.data.docs[i];
+                        department.add(
+                          DropdownMenuItem(
+                            child: Text(
+                              snap['name'],
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            value: "${snap.id}",
+                          ),
+                        );
+                      }
+                      return DropdownButton(
+                        style: TextStyle(color: Colors.black87),
+                        underline: Container(
+                          height: 2,
 
+                          color: Colors.green,
+                        ),
+                        icon: Icon(Icons.arrow_downward),
+                        items: department,
+                        onChanged: (orgs) async {
+                          final snackBar = SnackBar(
+                            content: Text(
+                              'Selected Organization value is a $orgs',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          );
+                          Scaffold.of(context).showSnackBar(snackBar);
+                          var num =await FirebaseFirestore.instance.collection("Organization").doc(selectedOrganization).collection("department").doc(orgs).collection("tokenNum").doc().get();
+                          //tokenNum = num.data()["tokenNum"];
+                          print(tokenNum);
+                          setState(() {
+                            selectedDepartment = orgs;
+                          });
+                        },
+                        value: selectedDepartment,
+                        isExpanded: true,
+                        hint: new Text(
+                          "Choose Department",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    }
+
+                  }
+              ),
 
               SizedBox(
                 height: 12,
@@ -151,7 +206,7 @@ class _InputTokenState extends State<InputToken> {
                   onPressed: () async {
                       if (_formkey.currentState.validate()){
                         // add token in organization
-                        await FirebaseFirestore.instance.collection("Organizations").doc(selectedOrganization)
+                        await FirebaseFirestore.instance.collection("Organizations").doc(selectedOrganization).collection("department").doc(selectedDepartment)
                         .collection("tokens").doc(tokenNum.toString()).set(
                           {
                             "name" : name.text,
@@ -170,6 +225,24 @@ class _InputTokenState extends State<InputToken> {
                         );
                         Scaffold.of(context)
                             .showSnackBar(SnackBar(content: Text('Token Generated')));
+                        showDialog(context: context,
+                            builder: (BuildContext context){
+                              return AlertDialog(
+                                title: Text("Token Generated Successfully"),
+                                content: Text("Your Token Num is $tokenNum"),
+                                actions: [
+                                  TextButton(onPressed: ()async {
+                                    Navigator.pop(context);
+                                    setState(() {
+
+                                    });
+                                  }, child: Text("Ok")),
+
+                                ],
+                              );
+                            }
+
+                        );
                       
                     return null;
                     }
